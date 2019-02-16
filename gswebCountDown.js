19bd8e74b30ef8a82f7259dcdf70e3c0bccd0838
Juan Jimenez <juanjimeneztj@gmail.com>
2019-02-15 16:27:21 -0800

    Initial commit - Countdown only with minutes and seconds
    This plugin need the library jquery

---
+++ gswebCountDown.js
@@ -0,0 +1,36 @@
+var gswebDoc = $(document),gswebCountDown = $('#gsweb-count'), gswebCountDownMinutes, gswebCountDownSeconds, counter;
+
+if(gswebCountDown.attr('data-minutes')){
+    gswebCountDownMinutes = gswebCountDown.attr('data-minutes');
+}else{
+    gswebCountDownMinutes = 0;
+} 
+if(gswebCountDown.attr('data-seconds')){
+    gswebCountDownSeconds = gswebCountDown.attr('data-seconds');
+}else{
+    gswebCountDownSeconds = 0;
+} 
+
+if (gswebCountDownMinutes < 10) { gswebCountDownMinutes = "0"+parseInt(gswebCountDownMinutes); } 
+if (gswebCountDownSeconds < 10){ gswebCountDownSeconds = "0"+parseInt(gswebCountDownSeconds); } 
+gswebCountDown.html(gswebCountDownMinutes+":"+gswebCountDownSeconds);
+
+gswebDoc.ready(function(){
+    counter = setInterval(function(){
+        if(gswebCountDownSeconds > 0){
+            gswebCountDownSeconds--;
+        }else{
+            if(gswebCountDownMinutes > 0){
+                gswebCountDownMinutes--;
+                gswebCountDownSeconds = 59;
+            }else{
+                clearInterval(counter);
+            }
+        }
+        if (gswebCountDownMinutes < 10) { gswebCountDownMinutes = "0"+parseInt(gswebCountDownMinutes); } 
+        if (gswebCountDownSeconds < 10){ gswebCountDownSeconds = "0"+parseInt(gswebCountDownSeconds); } 
+
+        gswebCountDown.html(gswebCountDownMinutes+":"+gswebCountDownSeconds);
+    },1000);
+
+});
